572c8a721f57dd25eadb8022a9adaf6f0acc8e1c
st1llwater <alokpunjofficial@gmail.com>
2023-02-15 13:07:06 +0530

    feat: draggable items component

---
+++ Frontend/components/Draggable.jsx
@@ -0,0 +1,41 @@
+import React, { MouseEvent, useRef, ReactElement } from 'react';
+
+const Draggable = ({ className, children }) => {
+  const slider = useRef < HTMLDivElement > null;
+
+  let mouseDown = false;
+  let startX, scrollLeft;
+
+  const startDragging = function (e) {
+    mouseDown = true;
+    startX = e.pageX - slider.current.offsetLeft;
+    scrollLeft = slider.current.scrollLeft;
+  };
+  const stopDragging = () => {
+    mouseDown = false;
+  };
+
+  function mouseMoveEvent(e) {
+    if (!mouseDown) {
+      return;
+    }
+    const x = e.pageX - slider.current.offsetLeft;
+    const scroll = x - startX;
+    slider.current.scrollLeft = scrollLeft - scroll;
+  }
+
+  return (
+    <div
+      ref={slider}
+      onMouseDown={startDragging}
+      onMouseUp={stopDragging}
+      onMouseLeave={stopDragging}
+      onMouseMove={mouseMoveEvent}
+      className={className}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Draggable;
